7cf427434a6aa40521487f51c5d89a7953d8348c
bright-webb <60761289+bright-webb@users.noreply.github.com>
2024-12-12 11:24:03 +0100

    Update Helper.php

--- src/Helper.php
+++ src/Helper.php
@@ -2,18 +2,25 @@
 namespace EasyRouter;
 class Helper {
     // Start session
-    public static function StartSession() {
-        if (session_status() == PHP_SESSION_NONE) {
-            session_start();
-        }
-    }
+   public static function Session($key) {
+    self::StartSession();
+    return $_SESSION[$key] ?? null;
+}
+
 
    public static function Asset($asset) {
     return $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] . '/' . ltrim($asset, '/');
 }
 
+    private static function ensureSessionStarted() {
+    if (session_status() === PHP_SESSION_NONE) {
+        session_start();
+    }
+}
+
 
     public static function Session($user){
+        
         if(isset($_SESSION[$user])){
             return $_SESSION[$user];
         } else {
